0f2247ddc6fc67c8aea6d003e0abba51a68edcb5
youp580 <youpvanhaagen@gmail.com>
2026-05-14 15:40:13 +0200

    interface toegevoegd

--- src/NotificationService.Application/Abstractions/IMessageProvider.cs
+++ src/NotificationService.Application/Abstractions/IMessageProvider.cs
@@ -2,6 +2,7 @@ namespace NotificationService.Application.Abstractions
 {
     public interface IMessageProvider
     {
-
+        //recipient can be email, phone number of deviceID
+        Task<string> SendMessageAsync(string recipient, string message, CancellationToken cancellationToken);
     }
 }
