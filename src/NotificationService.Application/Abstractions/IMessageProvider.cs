fe75af7b2b975c0bf8e6b1b05ff6fcf861b64a6f
Daniel van Ginneken <dcjvanginneken@gmail.com>
2026-05-16 02:08:11 +0200

    feat: add MessageFormat enum and update IMessageProvider with SupportedFormats

--- src/NotificationService.Application/Abstractions/IMessageProvider.cs
+++ src/NotificationService.Application/Abstractions/IMessageProvider.cs
@@ -1,8 +1,12 @@
+using NotificationService.Domain.Enums;
+
 namespace NotificationService.Application.Abstractions
 {
     public interface IMessageProvider
     {
-        //recipient can be email, phone number of deviceID
-        Task<string> SendAsync(string message, string recipient, IReadOnlyDictionary<string, string> credentials, CancellationToken ct);
+        IReadOnlySet<MessageFormat> SupportedFormats { get; }
+
+        // recipient can be email, phone number or deviceID depending on format
+        Task<string> SendAsync(MessageFormat format, string message, string recipient, IReadOnlyDictionary<string, string> credentials, CancellationToken ct);
     }
 }
